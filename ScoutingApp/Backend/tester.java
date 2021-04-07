9b8087af219ff4991ff0bcb33b5530627355790a
David Chen <davidhchen2003@gmail.com>
2021-04-07 19:55:43 -0400

    Created a method to process csv data and put it into a 2D array

---
+++ ScoutingApp/Backend/tester.java
@@ -0,0 +1,8 @@
+package Backend;
+
+public class tester{
+    public static void main(String[]args){
+        dataRetriever myDataRetriever = new dataRetriever();
+        System.out.println(myDataRetriever.getData("GreaterDC2018Scouting.csv"));
+    }
+}
